2c90ab95fd6f1493aa475ebaa587b4385ad68715
Ben Axnick <ben@axnick.com.au>
2011-02-01 13:17:13 +1100

    added bulk copy script

---
+++ bulk_copy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd $0
+
+hg clone file:///home/baxnick/code/_workspace/GripperBot
+
+hg clone file:///home/baxnick/SummerProject/levelhead/ARToolKitPlus/ARToolKitPlusTracker
+
+hg clone file:///home/baxnick/SummerProject/scripts
+
+hg clone file:///home/baxnick/SummerProject/levelhead/ARToolKitPlus/VisionSystem
+
+hg clone file:///home/baxnick/SummerProject/levelhead/ARToolKitPlus/id-markers/bch-copy/
+
